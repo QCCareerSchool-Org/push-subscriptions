6884de8f23c5fa16a4cb6e181bcd82be140f47e1
Dave Welsh <dave@qccareerschool.com>
2023-06-15 10:57:41 -0400

    Add a delay for testing

--- src/services/push/webPushPushService.ts
+++ src/services/push/webPushPushService.ts
@@ -7,6 +7,7 @@ export class WebPushPushService implements IPushService {
   public constructor(private readonly privateKey: string, private readonly publicKey: string) { /* empty */ }
 
   public async push(endpoint: string, p256dh: Buffer | null, auth: Buffer | null, heading: string, content: string, url: string | null): Promise<number> {
+    await new Promise(res => setTimeout(res, 10_000));
     return 200;
   }
 }
