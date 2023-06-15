fc0a5a982e6b8b9e2dd9e97fd84c5b7aeb5584f9
Dave Welsh <dave@qccareerschool.com>
2023-06-15 09:50:28 -0400

    Add website restriction

--- src/interactors/administrators/finalizeCampaignInteractor.ts
+++ src/interactors/administrators/finalizeCampaignInteractor.ts
@@ -65,7 +65,7 @@ SELECT DISTINCT ci.campaign_id, s.subscription_id, s.website_id, null, null, nul
 FROM subscriptions s
 JOIN subscriptions_interests si ON si.subscription_id = s.subscription_id
 JOIN campaigns_interests ci ON ci.interest_id = si.interest_id
-WHERE ci.campaign_id = ${campaignIdBin}`;
+WHERE s.website_id = ${campaign.websiteId} AND ci.campaign_id = ${campaignIdBin}`;
           } else {
             await t.$queryRaw`
 INSERT INTO sends
